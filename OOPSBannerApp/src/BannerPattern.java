37a1844d0b679ec951cd61bbe1cc5f7b47053f51
Hari Karthick <harikarthickr20@gmail.com>
2026-04-06 14:57:49 +0530

    feature/UC4: Render OOPS banner using string array and loop

--- OOPSBannerApp/src/BannerPattern.java
+++ OOPSBannerApp/src/BannerPattern.java
@@ -1,10 +1,16 @@
 public class BannerPattern {
 
-    public static final String LINE1 = String.join("   ", " *** ", " *** ", "*****", " ****");
-    public static final String LINE2 = String.join("   ", "*   *", "*   *", "*   *", "*    ");
-    public static final String LINE3 = String.join("   ", "*   *", "*   *", "*   *", "*    ");
-    public static final String LINE4 = String.join("   ", "*   *", "*   *", "*****", " ****");
-    public static final String LINE5 = String.join("   ", "*   *", "*   *", "*", "     *");
-    public static final String LINE6 = String.join("   ", "*   *", "*   *", "*", "     *");
-    public static final String LINE7 = String.join("   ", " *** ", " *** ", "*", " ****");
+    public static String[] getBannerLines() {
+        String[] lines = new String[7];
+
+        lines[0] = String.join("   ", " *** ", " *** ", "*****", " ****");
+        lines[1] = String.join("   ", "*   *", "*   *", "*   *", "*    ");
+        lines[2] = String.join("   ", "*   *", "*   *", "*   *", "*    ");
+        lines[3] = String.join("   ", "*   *", "*   *", "*****", " ****");
+        lines[4] = String.join("   ", "*   *", "*   *", "*", "     *");
+        lines[5] = String.join("   ", "*   *", "*   *", "*", "     *");
+        lines[6] = String.join("   ", " *** ", " *** ", "*", " ****");
+
+        return lines;
+    }
 }
--- OOPSBannerApp/src/BannerService.java
+++ OOPSBannerApp/src/BannerService.java
@@ -1,12 +1,10 @@
 public class BannerService {
 
     public void printBanner() {
-        System.out.println(BannerPattern.LINE1);
-        System.out.println(BannerPattern.LINE2);
-        System.out.println(BannerPattern.LINE3);
-        System.out.println(BannerPattern.LINE4);
-        System.out.println(BannerPattern.LINE5);
-        System.out.println(BannerPattern.LINE6);
-        System.out.println(BannerPattern.LINE7);
+        String[] lines = BannerPattern.getBannerLines();
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }

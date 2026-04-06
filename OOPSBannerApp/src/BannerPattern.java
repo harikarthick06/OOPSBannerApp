f30937ce4840152e4acec0ec92e12d9e55e7dd5c
Hari Karthick <harikarthickr20@gmail.com>
2026-04-06 15:33:46 +0530

    feature/UC7 q

--- OOPSBannerApp/src/BannerPattern.java
+++ OOPSBannerApp/src/BannerPattern.java
@@ -1,7 +1,25 @@
 public class BannerPattern {
 
-    public static String[] getOPattern() {
-        return new String[] {
+    static class CharacterPattern {
+        private char character;
+        private String[] pattern;
+
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    public static CharacterPattern getOPattern() {
+        return new CharacterPattern('O', new String[] {
             " *** ",
             "*   *",
             "*   *",
@@ -9,23 +27,23 @@ public class BannerPattern {
             "*   *",
             "*   *",
             " *** "
-        };
+        });
     }
 
-    public static String[] getPPattern() {
-        return new String[] {
+    public static CharacterPattern getPPattern() {
+        return new CharacterPattern('P', new String[] {
             "*****",
             "*   *",
             "*   *",
             "*****",
-            "*",
-            "*",
-            "*"
-        };
+            "*    ",
+            "*    ",
+            "*    "
+        });
     }
 
-    public static String[] getSPattern() {
-        return new String[] {
+    public static CharacterPattern getSPattern() {
+        return new CharacterPattern('S', new String[] {
             " ****",
             "*    ",
             "*    ",
@@ -33,18 +51,23 @@ public class BannerPattern {
             "    *",
             "    *",
             " ****"
-        };
+        });
     }
 
     public static String[] getBannerLines() {
-        String[] oPattern = getOPattern();
-        String[] pPattern = getPPattern();
-        String[] sPattern = getSPattern();
+        CharacterPattern o1 = getOPattern();
+        CharacterPattern o2 = getOPattern();
+        CharacterPattern p = getPPattern();
+        CharacterPattern s = getSPattern();
 
         String[] lines = new String[7];
 
         for (int i = 0; i < 7; i++) {
-            lines[i] = String.join("   ", oPattern[i], oPattern[i], pPattern[i], sPattern[i]);
+            lines[i] = String.join("   ",
+                    o1.getPattern()[i],
+                    o2.getPattern()[i],
+                    p.getPattern()[i],
+                    s.getPattern()[i]);
         }
 
         return lines;

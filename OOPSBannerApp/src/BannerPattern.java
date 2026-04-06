612d5411cfc5c22b8c95d02264f47be7fcc991cb
Hari Karthick <harikarthickr20@gmail.com>
2026-04-06 14:35:52 +0530

    feature/UC2

--- OOPSBannerApp/src/BannerPattern.java
+++ OOPSBannerApp/src/BannerPattern.java
@@ -1,3 +1,10 @@
 public class BannerPattern {
-    public static final String MESSAGE = "OOPS";
+
+    public static final String LINE1 = "  ***      ***    ******    ***** ";
+    public static final String LINE2 = " **  **   **  **  **   **  **     ";
+    public static final String LINE3 = "**    ** **    ** **   **  **     ";
+    public static final String LINE4 = "**    ** **    ** ******    ***   ";
+    public static final String LINE5 = "**    ** **    ** **           ** ";
+    public static final String LINE6 = " **  **   **  **  **           ** ";
+    public static final String LINE7 = "  ***      ***    **       *****  ";
 }
--- OOPSBannerApp/src/BannerService.java
+++ OOPSBannerApp/src/BannerService.java
@@ -1,6 +1,12 @@
 public class BannerService {
 
-    public void printMessage() {
-        System.out.println(BannerPattern.MESSAGE);
+    public void printBanner() {
+        System.out.println(BannerPattern.LINE1);
+        System.out.println(BannerPattern.LINE2);
+        System.out.println(BannerPattern.LINE3);
+        System.out.println(BannerPattern.LINE4);
+        System.out.println(BannerPattern.LINE5);
+        System.out.println(BannerPattern.LINE6);
+        System.out.println(BannerPattern.LINE7);
     }
 }
